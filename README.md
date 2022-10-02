09a7bc96dbc109dfb243ee0a4f03b10dd3e784b5
Veronika Kopena <vkopena@gmail.com>
2022-10-02 23:50:13 +0300

    New project_qa_python

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # qa_python
+
+def test_set_book_rating_5_positive_case(self):
+1.Устанавливаем допустимый рейтинг (позитивный кейс) 
+
+def test_set_book_rating_12_negative_case(self):
+2.Устанавливаем недопустимый рейтинг (негативный кейс) 
+
+def test_get_book_rating_equal_name_of_book(self):
+3.Проверяем, что рейтинг книги можно получить по её названию
+
+def test_get_books_with_specific_rating_is_list(self):
+4.Проверяем, что книги с определенным рейтингом хранятся в списке
+
+def test_get_books_rating_is_dictionary(self):
+5.Проверяем, что books_rating - это словарь
+
+def test_add_book_in_favorites_list_is_not_empty(self):
+6.Проверяем, что книга из словаря добавлена в список "Избранное"
+
+def test_delete_book_from_favorites_list_is_empty(self):
+7.Проверяем, что после удаления книги список пуст
+
+def test_get_list_of_favorites_books_is_list(self):
+8.Проверяем, что список Избранных книг непустой список
+
--- tests.py
+++ tests.py
@@ -22,3 +22,45 @@ class TestBooksCollector:
 
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+
+
+    def test_set_book_rating_5_positive_case(self):
+        collector = BooksCollector()
+        collector.set_book_rating =='5'
+        assert collector.set_book_rating == 'This is correct value'
+
+    def test_set_book_rating_12_negative_case(self):
+        collector = BooksCollector()
+        collector.set_book_rating =='12'
+        assert collector.set_book_rating == 'This is an error'
+
+    def test_get_book_rating_equal_name_of_book(self):
+        collector = BooksCollector()
+        collector.books_rating.get(self.name)
+        assert collector.get_book_rating[0] == self.name[0]
+
+    def test_get_books_with_specific_rating_is_list(self):
+        collector = BooksCollector()
+        collector.books_with_specific_rating = []
+        assert collector.get_books_with_specific_rating(self.books_with_specific_rating) == 'This list!'
+
+    def test_get_books_rating_is_dictionary(self):
+        collector = BooksCollector()
+        collector.books_rating = {}
+        assert collector.get_books_rating() == 'This dictionary!'
+
+    def test_add_book_in_favorites_list_is_not_empty(self):
+        collector = BooksCollector()
+        collector.add_new_book_in_favorites('Гордость и предубеждение и зомби')
+        assert len(collector.add_book_in_favorites()) == 1
+
+    def test_delete_book_from_favorites_list_is_empty(self):
+        collector = BooksCollector()
+        collector.favorites = []
+        assert collector.delete_book_from_favorites() == 'List is empty'
+
+    def test_get_list_of_favorites_books_is_list(self):
+        collector = BooksCollector()
+        collector.get_list_of_favorites_books()
+        assert collector.get_list_of_favorites_books() == 'List is not empty'
+
